075535fa018f00286eb0150778b15a3a62afd019
withlqs <withlqs@gmail.com>
2015-07-08 00:44:20 +0800

    Add model in StuInfo.models

--- StuInfo/models.py
+++ StuInfo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    StudentID = models.CharField(max_length = 15)
+    Name = models.CharField(max_length = 30)
+    PhoneNumber = models.CharField(max_length = 20)
+    Email = models.EmailField()
+    Address = models.CharField(max_length = 100)
+    Birthday = models.DateField()
+
+class User(models.Model):
+    UserID = models.CharField(max_length = 20)
+    Type = models.SmallIntegerField()
+    PasswordHash = models.CharField(max_length = 50)
+    Email = models.EmailField()
+    
